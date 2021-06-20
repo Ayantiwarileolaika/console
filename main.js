37dd3b2a0bd5ae671f587f409ddd922dd86e74a6
Ayantiwarileolaika <72645383+Ayantiwarileolaika@users.noreply.github.com>
2021-06-20 13:00:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log ("This is my first simple message");
+var fname="Ayan";
+var sname="Tiwari";
+console.log(fname);
+console.log(sname);
+var fullname = fname.concat(sname);
+console.log(fullname);
+var newname = fullname.replace("AyanTiwari","NoopurTiwari");
+console.log(newname);
+console.log(newname.toUpperCase());
+console.log(newname.toLowerCase());
+console.log(fullname.toUpperCase());
+console.log(fullname.toLowerCase());
+var s_root = Math.sqrt(16); 
+console.log(s_root)
+var maximum = Math.max(10,15)
+console.log(maximum)
+var minimum = Math.min(5,10)
+console.log(minimum)
+var power = Math.pow(15,2)
+console.log(power)
+var bignumber = 36.23456789012345000000000000;
+console.log(bignumber.toFixed(1))
